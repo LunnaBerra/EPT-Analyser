45f5b7ff9a6ae28c37efc21af74917d485903139
LunnaBerra <76743430+LunnaBerra@users.noreply.github.com>
2022-11-18 15:42:29 +0100

    V1

---
+++ Scraper.py
@@ -0,0 +1,30 @@
+from os import stat
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+URL_list = ["https://www.premierleague.com/players/3960/Harry-Kane/stats"]
+for url in URL_list:
+    driver.get(url)
+    print(driver.title)
+
+    try:
+        main = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "statsListBlock"))
+        )
+        stats = main.text
+        x = stats.split("\n")
+        statDict =
+
+        print(x)
+
+    except:
+        driver.quit()
+
+    driver.quit
