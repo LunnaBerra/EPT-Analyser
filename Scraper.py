d4d821ef5781908a4b856ef925d07da265270446
LunnaBerra <76743430+LunnaBerra@users.noreply.github.com>
2022-11-18 18:27:10 +0100

    V3

--- Scraper.py
+++ Scraper.py
@@ -6,30 +6,107 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
-URL_list = ["https://www.premierleague.com/players/3960/Harry-Kane/stats"]
-for url in URL_list:
-    driver.get(url)
-    print(driver.title)
-
-    try:
-        main = WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "statsListBlock"))
-        )
-        stats = main.text
-        x = stats.split("\n")
-        statList = list(x)
-        header = statList.pop(0)
+import tabulate
+def scoreTeam(playerURLList):
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+    URL_list = playerURLList
+    playerList = []
+    cookies = False
+    for url in URL_list:
+        driver.get(url)
+        if not cookies:
+            time.sleep(5)
+            cookies = True
         statDict = {}
-        while len(statList) > 1:
-            s = statList.pop(0)
-            statDict[s] = statList.pop(0)
-        print(statDict)
+        position = ""
+        name = ""
+        number = ""
+        score = 0
+    # name t-colour
+        try:
+            main = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "playerDetails"))
+            )
+            stats = main.text
+            x = stats.split("\n")
+            number = x[0]
+            name = x[1]
+        except:
+            driver.quit()
+        try:
+            main = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "fixedSidebar"))
+            )
 
-    except:
-        driver.quit()
+            stats = main.text
+            x = stats.split("\n")
+            statList = list(x)
+            Ok = False
+            for i in statList:
+                if Ok == True:
+                    position = i
+                    break
 
-    driver.quit
+                if i == "Position":
+                    Ok = True
+        except:
+            driver.quit()
+
+        try:
+            # statsListBlock
+            main = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "playerStats"))
+            )
+            stats = main.text
+            x = stats.split("\n")
+            statList = list(x)
+            #header = statList.pop(0)
+
+            while len(statList) > 1:
+                NoN = False
+                head = statList.pop(0)
+                if statDict.__contains__(head):
+                    statList.pop(0)
+                    continue
+                value = statList.pop(0)
+                x = value.split(".")
+                x = "".join(x)
+                y = value.split(",")
+                y = "".join(y)
+                if y.isnumeric():
+                    value = y
+                k = value.split("%")
+                k = "".join(k)
+                if k.isnumeric():
+                    value = k
+                if not x.isnumeric() and not y.isnumeric() and not k.isnumeric():
+                    statList.insert(0, value)
+                    NoN = True
+                if NoN is True:
+                    continue
+                statDict[head] = value
+
+        except:
+            driver.quit()
+
+        if position == "Forward":
+            score = float(statDict["Goals"])/float(statDict["Appearances"])
+        elif position == "Defender":
+            pass
+        elif position == "Midfielder":
+            pass
+        elif position == "Goalkeeper":
+            pass
+        else:
+            pass
+
+        playerSum = {"Name": name, "Number": number,
+                    "Position": position, "Score": score}
+
+        playerList.append(playerSum)
+        driver.quit
+
+    # print(playerList)
+    return playerList
+
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import Scraper
+import PlayerURLs
+import tabulate
+
+
 def main():
-    print("Hello World!")
+
+    URL1 = PlayerURLs.getList("Tottenham")
+    playerList = Scraper.scoreTeam(URL1)
+
+    header = playerList[0].keys()
+    rows = [x.values() for x in playerList]
+    print(tabulate.tabulate(rows, header))
 
 if __name__ == "__main__":
     main()
